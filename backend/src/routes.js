f983ead0044ca9f70cdb7dfe81717dfb6e8d1fa5
ElandVarse <andre.jobs23@gmail.com>
2020-03-30 11:41:10 -0300

    Backend Fix

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,23 +1,21 @@
 const express = require('express');
 
-const ongController = require('./controllers/ongController')
-const incidentController = require('./controllers/incidentController')
-const profileController = require('./controllers/profileController')
-const sessionController = require('./controllers/sessionController')
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfilerController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router();
 
-routes.get('/ongs', ongController.index)
-routes.post('/ongs', ongController.create)
+routes.post('/sessions', SessionController.create)
 
-routes.get('/incidents', incidentController.index)
-routes.post('/incidents', incidentController.create)
-routes.delete('/incidents/:id', incidentController.delete)
+routes.get('/ongs', OngController.index );
+routes.post('/ongs', OngController.create);
 
-routes.get('/profile', profileController.index)
+routes.get('/profile', ProfilerController.index);
 
-routes.post('/sessions', sessionController.create)
-
-
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Logon.css'
+
+import { Link } from 'react-router-dom'
+import {FiLogIn} from 'react-icons/fi'
+
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+export default function Logon(){
+    return(
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+
+                <form>
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder='Sua ID'/>
+                    <button className='button'type='submit'>Entrar</button>
+
+                    <Link className='back-link' to="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho cadastro.
+                    </Link>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+    )
+}
